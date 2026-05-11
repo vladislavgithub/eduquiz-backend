125376378eab1803631faceb23e4545cba6dae9e
vladik <dvor19821@gmail.com>
2026-05-11 09:37:19 +0300

    rebuild Flutter web bundle with relative API URLs

--- web/flutter_service_worker.js
+++ web/flutter_service_worker.js
@@ -3,11 +3,11 @@ const MANIFEST = 'flutter-app-manifest';
 const TEMP = 'flutter-temp-cache';
 const CACHE_NAME = 'flutter-app-cache';
 
-const RESOURCES = {"flutter_bootstrap.js": "bd2087e2a974d5f4c208f6636da8c140",
+const RESOURCES = {"flutter_bootstrap.js": "6617e8e37ebcec24a6cbea354666588f",
 "version.json": "ec90f1a19f7270df481c0c8fb266f323",
 "index.html": "9b1b42429df49b40dcb70597dc13a282",
 "/": "9b1b42429df49b40dcb70597dc13a282",
-"main.dart.js": "de07521f5c7dcfd058ca8ebe8d4d0ae6",
+"main.dart.js": "28ac48c0a929bf313189c808e936cd2f",
 "flutter.js": "888483df48293866f9f41d3d9274a779",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
