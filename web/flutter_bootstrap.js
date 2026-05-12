c838065191ee4101571d9b24849acb45d84d6fdb
vladik <dvor19821@gmail.com>
2026-05-12 22:39:48 +0300

    fix: upload multipart boundary; clickable stats tiles; admin courses full CRUD with banks/questions

--- web/flutter_bootstrap.js
+++ web/flutter_bootstrap.js
@@ -38,6 +38,6 @@ _flutter.buildConfig = {"engineRevision":"035316565ad77281a75305515e4682e6c4c6f7
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "278173518"
+    serviceWorkerVersion: "3277038853"
   }
 });
--- web/flutter_service_worker.js
+++ web/flutter_service_worker.js
@@ -3,11 +3,11 @@ const MANIFEST = 'flutter-app-manifest';
 const TEMP = 'flutter-temp-cache';
 const CACHE_NAME = 'flutter-app-cache';
 
-const RESOURCES = {"flutter_bootstrap.js": "cb309d5996afe0bd19faa679f715ba28",
+const RESOURCES = {"flutter_bootstrap.js": "54f6d6da3c08307ec98f8ee1e13ef8ef",
 "version.json": "ec90f1a19f7270df481c0c8fb266f323",
 "index.html": "9b1b42429df49b40dcb70597dc13a282",
 "/": "9b1b42429df49b40dcb70597dc13a282",
-"main.dart.js": "dff575552271afdafdae24ce78579ec8",
+"main.dart.js": "f0a7e155aae0fa76a6275d112f5ba763",
 "flutter.js": "888483df48293866f9f41d3d9274a779",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
@@ -22,7 +22,7 @@ const RESOURCES = {"flutter_bootstrap.js": "cb309d5996afe0bd19faa679f715ba28",
 "assets/packages/cupertino_icons/assets/CupertinoIcons.ttf": "1a051556fd556745b6ed2e7e1ee828a3",
 "assets/shaders/ink_sparkle.frag": "ecc85a2e95f5e9f53123dcaf8cb9b6ce",
 "assets/AssetManifest.bin": "693635b5258fe5f1cda720cf224f158c",
-"assets/fonts/MaterialIcons-Regular.otf": "cfa0a6a28f520bef0d39cf1630fcdd5f",
+"assets/fonts/MaterialIcons-Regular.otf": "7aa6858d11560fd91b925d1e1ccd9fd2",
 "canvaskit/skwasm.js": "1ef3ea3a0fec4569e5d531da25f34095",
 "canvaskit/skwasm_heavy.js": "413f5b2b2d9345f37de148e2544f584f",
 "canvaskit/skwasm.js.symbols": "0088242d10d7e7d6d2649d1fe1bda7c1",
