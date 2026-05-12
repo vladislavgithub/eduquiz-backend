aa21e5da592748b33ccbde29bf440e464ce61408
vladik <dvor19821@gmail.com>
2026-05-13 00:24:59 +0300

    fix(admin): no overlay, teacher courses expand+bank nav, filter chips, Преподаватель

--- web/flutter_service_worker.js
+++ web/flutter_service_worker.js
@@ -3,11 +3,11 @@ const MANIFEST = 'flutter-app-manifest';
 const TEMP = 'flutter-temp-cache';
 const CACHE_NAME = 'flutter-app-cache';
 
-const RESOURCES = {"flutter_bootstrap.js": "b849f7362ea0cd7e883f5f7c58ed85b5",
+const RESOURCES = {"flutter_bootstrap.js": "5b30bbaef415ee9c43a6bcad5a28ec34",
 "version.json": "ec90f1a19f7270df481c0c8fb266f323",
 "index.html": "9b1b42429df49b40dcb70597dc13a282",
 "/": "9b1b42429df49b40dcb70597dc13a282",
-"main.dart.js": "8d64979c6ef3dc0597b6c9616814e207",
+"main.dart.js": "1469c46a62c70ac6e2efa17383fdc7f3",
 "flutter.js": "888483df48293866f9f41d3d9274a779",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
