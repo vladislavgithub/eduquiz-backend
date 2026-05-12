92b88b7e94296f7ecad961d743d542ec6ad79852
vladik <dvor19821@gmail.com>
2026-05-12 22:00:10 +0300

    fix: build absolute image URLs from Uri.base when API_BASE_URL is empty

--- web/flutter_service_worker.js
+++ web/flutter_service_worker.js
@@ -3,11 +3,11 @@ const MANIFEST = 'flutter-app-manifest';
 const TEMP = 'flutter-temp-cache';
 const CACHE_NAME = 'flutter-app-cache';
 
-const RESOURCES = {"flutter_bootstrap.js": "1fec76d9cbf313fa865503a3455f8c84",
+const RESOURCES = {"flutter_bootstrap.js": "a73c9c3fec3fe23edd37677b9142985d",
 "version.json": "ec90f1a19f7270df481c0c8fb266f323",
 "index.html": "9b1b42429df49b40dcb70597dc13a282",
 "/": "9b1b42429df49b40dcb70597dc13a282",
-"main.dart.js": "fc903897e8929ec60840d0a9b78ee3ca",
+"main.dart.js": "6c5d06b8f8550d26962501999d664f6d",
 "flutter.js": "888483df48293866f9f41d3d9274a779",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
