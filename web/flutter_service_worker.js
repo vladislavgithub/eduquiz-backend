f5ec063fffa80be1dfd6c712882724100da05091
vladik <dvor19821@gmail.com>
2026-05-12 22:25:33 +0300

    fix: use AlertDialog for edit name dialog (proper width on web)

--- web/flutter_service_worker.js
+++ web/flutter_service_worker.js
@@ -3,11 +3,11 @@ const MANIFEST = 'flutter-app-manifest';
 const TEMP = 'flutter-temp-cache';
 const CACHE_NAME = 'flutter-app-cache';
 
-const RESOURCES = {"flutter_bootstrap.js": "f2a0600577a97512d2469efe4538db77",
+const RESOURCES = {"flutter_bootstrap.js": "cb309d5996afe0bd19faa679f715ba28",
 "version.json": "ec90f1a19f7270df481c0c8fb266f323",
 "index.html": "9b1b42429df49b40dcb70597dc13a282",
 "/": "9b1b42429df49b40dcb70597dc13a282",
-"main.dart.js": "16286e6d294f5e9eee4b1b70c603c7bd",
+"main.dart.js": "dff575552271afdafdae24ce78579ec8",
 "flutter.js": "888483df48293866f9f41d3d9274a779",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
