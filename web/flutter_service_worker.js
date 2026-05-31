b4511f747047c4a92c988bce1d05b003a85d00d5
vladik <dvor19821@gmail.com>
2026-05-31 12:36:01 +0300

    leave-beacon: фронт-бандл с sendBeacon фиксом (пустой Blob)

--- web/flutter_service_worker.js
+++ web/flutter_service_worker.js
@@ -3,11 +3,11 @@ const MANIFEST = 'flutter-app-manifest';
 const TEMP = 'flutter-temp-cache';
 const CACHE_NAME = 'flutter-app-cache';
 
-const RESOURCES = {"flutter_bootstrap.js": "dc4d2645e82492ea1144e2987127b817",
+const RESOURCES = {"flutter_bootstrap.js": "32568a7be3178ce036b35c79b19598df",
 "version.json": "ec90f1a19f7270df481c0c8fb266f323",
 "index.html": "9b1b42429df49b40dcb70597dc13a282",
 "/": "9b1b42429df49b40dcb70597dc13a282",
-"main.dart.js": "f61234df66b16628b1ab5c272106fd27",
+"main.dart.js": "9ef7aa69d3557d81a01e1e8198f160b2",
 "flutter.js": "888483df48293866f9f41d3d9274a779",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
